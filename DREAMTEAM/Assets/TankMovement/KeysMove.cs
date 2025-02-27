3f3eb634d1d8bea8282431ebc315e6cef1d12989
MaximoAlbero <maximo.albero@enti.cat>
2025-02-27 19:33:54 +0100

    Codigos de movimiento wasd + keys
    He añadido los 2 scripts que gestionan el movimiento de los tanques, uno usa las flechas y el otro wasd.

---
+++ DREAMTEAM/Assets/TankMovement/KeysMove.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+//No se si este codigo sera el final, solo es para tener algo que se mueva
+[RequireComponent(typeof(Rigidbody2D))]
+public class KeysMove : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float RotationSpeed;
+
+    private Rigidbody2D rb;
+
+    //kinda wish this wasnt here but couldn't think of another way
+    //float rotation = 0;
+
+    float movementX;
+    float movementY;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        movementX = 0;
+        movementY = 0;
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(movementX * speed * Time.deltaTime, movementY * speed + Time.deltaTime);
+
+        float horizontalInput = Input.GetAxisRaw("HorizontalKeys");
+        float verticalInput = Input.GetAxisRaw("VerticalKeys");
+
+        Vector2 movementDirection = new Vector2(horizontalInput, verticalInput);
+        float inputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
+        movementDirection.Normalize();
+
+        transform.Translate(movementDirection * speed * inputMagnitude * Time.deltaTime, Space.World);
+
+        if (movementDirection != Vector2.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, movementDirection);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, RotationSpeed * Time.deltaTime);
+        }
+
+        //if (Input.GetAxis("HorizontalAD")) //Input.GetKey("d")
+        //{
+        //    movementX = 1;
+        //}else if (Input.GetKey("a") || Input.GetKey("left"))
+        //{
+        //    movementX = -1;
+        //} else
+        //{
+        //    movementX = 0;
+        //}
+
+        //if (Input.GetKey("w") || Input.GetKey("up"))
+        //{
+        //    movementY = 1;
+        //} else if (Input.GetKey("s") || Input.GetKey("down"))
+        //{
+        //    movementY = -1;
+        //} else
+        //{
+        //    movementY = 0;
+        //}
+    }
+}
