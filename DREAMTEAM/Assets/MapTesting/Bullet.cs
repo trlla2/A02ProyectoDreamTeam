692be0a455f080eb24d4aac3aef019b8289d5c90
Enti-Xuezhi <xuezhi.wang@enti.cat>
2025-03-10 14:11:21 +0100

    power up hecho
    error de bala

--- DREAMTEAM/Assets/MapTesting/Bullet.cs
+++ DREAMTEAM/Assets/MapTesting/Bullet.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    //initialize variables for prefab bullet
+    [SerializeField] public float speed = 1000000f;
+    [SerializeField] public Rigidbody2D rb;
+    [SerializeField] public float bounceTime = 3f;
 
-    public float speed = 10f;
-    public Rigidbody2D rb;
-
-
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        rb.velocity = transform.up* speed;
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
+    void Start()
+    {
+        rb.velocity = transform.forward * speed; //give direction to bullet when initiate
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log(rb.IsAwake());
+
+        //Debug.Log(rb.velocity);
+    }
+
+    private void OnCollisionEnter2D (Collision2D targetHit)
+    {
+        Debug.Log ("Impacto");
+        if (bounceTime == 0f)
+        {
+            Destroy(gameObject); //if bounce times is 0, destroy the bullet
+        }
+        else
+        {
+            var contact = targetHit.GetContact(0); //seeks for contact
+            Vector2 newDirection = Vector2.Reflect(contact.normal, rb.velocity.normalized); //calculate the direction of the bullet that it has to bounce
+            bounceTime--; //minus bounce time until it destroys
+        }
+    }
 }
