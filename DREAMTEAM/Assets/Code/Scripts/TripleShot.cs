079776361fa20b7b1777ed7665d5a427b8c638f1
Maximo Albero Fernandez <maximo.albero@enti.cat>
2025-03-11 13:33:21 +0100

    Started Triple Shot

---
+++ DREAMTEAM/Assets/Code/Scripts/TripleShot.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PowerUps/TripleShot")]
+public class TripleShot : PowerUpEffect
+{
+    public override void Apply(GameObject target)
+    {
+
+    }
+}
