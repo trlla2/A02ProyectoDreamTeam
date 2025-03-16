ecc10242e43868754f4562477ccc903d7892486d
MaximoAlbero <maximo.albero@enti.cat>
2025-03-16 23:08:41 +0100

    cahnges in the code of TripleShot

--- DREAMTEAM/Assets/Code/Scripts/TripleShot.cs
+++ DREAMTEAM/Assets/Code/Scripts/TripleShot.cs
@@ -5,8 +5,19 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "PowerUps/TripleShot")]
 public class TripleShot : PowerUpEffect
 {
+    public Transform firePoint;
+    public GameObject bulletSprite;
+
+    [SerializeField] float spreadAngle = 15f;
     public override void Apply(GameObject target)
     {
+        target.GetComponent<Bullet>();
+        for (int i = 0; i < 3; i++)
+        {
+            target = Instantiate(bulletSprite, firePoint.position, firePoint.rotation);
+            target.transform.SetParent(firePoint);
+            target.transform.Rotate(0f, spreadAngle * (i - 1), 0f);
 
+        }
     }
 }
