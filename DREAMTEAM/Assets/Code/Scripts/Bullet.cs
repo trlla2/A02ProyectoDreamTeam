aa301f73ff0ebdfdeea8e9f1fb4b96bb6b7c4107
Enti-Xuezhi <xuezhi.wang@enti.cat>
2025-03-16 23:18:46 +0100

    in game player life count

--- DREAMTEAM/Assets/Code/Scripts/Bullet.cs
+++ DREAMTEAM/Assets/Code/Scripts/Bullet.cs
@@ -11,12 +11,6 @@ public class Bullet : MonoBehaviour
     [SerializeField] public Rigidbody2D rb;
     [SerializeField] public float bounceTime = 3f;
 
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-
-    }
-
     void Start()
     {
         rb.velocity = transform.up * speed; //give direction to bullet when initiate
