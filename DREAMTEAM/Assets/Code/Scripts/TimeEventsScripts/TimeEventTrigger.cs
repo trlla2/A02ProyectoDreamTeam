75127c9548549fa0616575084af0017acfee7b9a
Enti-Xuezhi <xuezhi.wang@enti.cat>
2025-05-21 01:04:31 +0200

    fix

--- DREAMTEAM/Assets/Code/Scripts/TimeEventsScripts/TimeEventTrigger.cs
+++ DREAMTEAM/Assets/Code/Scripts/TimeEventsScripts/TimeEventTrigger.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class TestTimeEvent : MonoBehaviour
 {
-    [Header("Effect Settings")]
-    [SerializeField] private float minDuration = 10f;
-    [SerializeField] private float maxDuration = 20f;
-    private TankControlModifier tankControlModifier;
 
         [SerializeField] private float slowSpeed = 0.5f; // 50% speed
         [SerializeField] private float fastSpeed = 1.5f; // 150% speed
@@ -17,27 +13,10 @@ public class TestTimeEvent : MonoBehaviour
             // Create event with random speed modification
             //TimeEvent.Create(OnEventComplete, eventDuration, slowSpeed, fastSpeed);
         }
-    }
 
-    private void OnEffectComplete()
-    {
-        Debug.Log("Tank effect ended - controls returned to normal");
-    }
-
-    // Call this to trigger a new random tank effect
-    public void TriggerRandomEffect()
-    {
-        float randomDuration = Random.Range(minDuration, maxDuration);
-        Debug.Log($"Triggering random tank effect for {randomDuration} seconds");
-
-        if (tankControlModifier != null)
+        private void OnEventComplete()
         {
-            tankControlModifier.ApplyRandomEffect(randomDuration);
-
-            TimeEvent.Create(
-                OnEffectComplete,
-                randomDuration
-            );
+            Debug.Log("Speed event ended - returned to normal speed");
         }
 
         // Call this to trigger a new random speed event
@@ -45,5 +24,4 @@ public class TestTimeEvent : MonoBehaviour
         {
             TimeEvent.Create(OnEventComplete, duration, slowSpeed, fastSpeed);
         }
-    }
 }
